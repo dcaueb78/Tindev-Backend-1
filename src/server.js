ea5121cd4f0e15b7f8ef3d8bd54204f4aeae867f
Bruno Silva <bruno3dcontato@gmail.com>
2019-08-08 06:09:50 -0300

    start
    aula 03

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-culqu.mongodb.net/omnistack?retryWrites=true&w=majority', {
+	useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
